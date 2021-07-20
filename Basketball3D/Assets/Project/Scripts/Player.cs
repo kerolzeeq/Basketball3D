54afa792633a9aaf7fe991c4b3739359f0548312
kerolzeeq <kerolzeeq@gmail.com>
2021-07-20 17:11:58 +0800

    Ball throwing script done

---
+++ Basketball3D/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject playercamera;
+
+    public float ballThrowingForce = 300f;
+
+    private bool holdingBall = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall)
+        {
+            ball.transform.position = playercamera.transform.position + playercamera.transform.forward * 2; // ball move with player camera
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playercamera.transform.forward * ballThrowingForce);
+            }
+        }
+    }
+}
